f454ed2eaa4b49da89c6d5235e4ccce254906dcb
chrishaig14 <chrishaig14@gmail.com>
2019-06-25 17:06:29 -0300

    Add requests to server in LoginView

--- src/LoginView.js
+++ src/LoginView.js
@@ -5,9 +5,11 @@ import {Link, Redirect} from "react-router-dom";
 
 class LoginView extends React.Component {
 
+    serverUrl = "http://localhost:8888";
+
     constructor(props) {
         super(props);
-        this.state = {username: "", password: "", toFeed: false};
+        this.state = {username: "", password: "", toFeed: false, error: false};
         this.submitLogin = this.submitLogin.bind(this);
         this.onUsernameChange = this.onUsernameChange.bind(this);
         this.onPasswordChange = this.onPasswordChange.bind(this);
@@ -22,8 +24,27 @@ class LoginView extends React.Component {
     }
 
     submitLogin(event) {
+
         console.log(`Submitting login with username=${this.state.username} and password=${this.state.password}`);
-        this.setState({toFeed: true});
+        fetch(this.serverUrl + "/login", {
+            method: "POST",
+            body: JSON.stringify(
+                {
+                    username: this.state.username, password: this.state.password
+                }
+            )
+        }).then(
+            (res) => {
+                if (!res.ok) {
+                    this.setState({error: "There was an error!"});
+                } else {
+                    this.setState({toFeed: true});
+                }
+            }
+        ).catch(
+            () => this.setState({error: "Could not process request!"})
+        );
+        event.preventDefault();
     }
 
     render() {
@@ -37,6 +58,7 @@ class LoginView extends React.Component {
                     <label>USERNAME<input type="text" value={this.state.username}
                                           onChange={this.onUsernameChange}/></label>
                     <label>PASSWORD<input type="password" value={this.state.password} onChange={this.onPasswordChange}/></label>
+                    {(this.state.error ? <span style={{color: "red"}}>{this.state.error}</span> : null)}
                     <div className={"login-buttons"}>
                         <button type="submit">Login</button>
                         <Link to={"/signup"} className="button-link">Sign up</Link>
