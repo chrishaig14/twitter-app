e18dd607145754398ef1cca1ce88cb1c47386f49
chrishaig14 <chrishaig14@gmail.com>
2019-07-02 21:24:36 -0300

    Add user pic loading in Comment

--- src/Comment.js
+++ src/Comment.js
@@ -1,16 +1,30 @@
 import React from "react";
 
 class Comment extends React.Component {
+    serverUrl = "http://localhost:8888";
+
     constructor(props) {
         super(props);
-        console.log("CONSTRUCTNG COMMENT WITH: ", this.props.data)
+        console.log("CONSTRUCTNG COMMENT WITH: ", this.props.data);
+        this.state = {userpic: ""};
+    }
+
+    componentDidMount() {
+        fetch(this.serverUrl + "/users/" + this.props.data.username + "/img",
+            {method: "GET"}).then(
+            res => res.text()
+        ).then(
+            res => {
+                this.setState({userpic: res});
+            }
+        );
     }
 
     render() {
         return (
             <div className={"comment"}>
                 {/*<h1>Comment</h1>*/}
-                <div className={"comment-user-pic"}></div>
+                <img className={"comment-user-pic"} src={this.state.userpic}/>
                 <a href="" className={"comment-user"}>{this.props.data.username}</a>
                 <div className={"comment-content"}>{this.props.data.content}</div>
             </div>
