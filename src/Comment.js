38af6728b221d8f3ab844fd93f8b233b7cc24fa8
chrishaig14 <chrishaig14@gmail.com>
2019-07-05 20:36:07 -0300

    Add NavLink to UserView in Comment

--- src/Comment.js
+++ src/Comment.js
@@ -1,4 +1,5 @@
 import React from "react";
+import {NavLink} from "react-router-dom";
 
 class Comment extends React.Component {
     serverUrl = "http://localhost:8888";
@@ -25,7 +26,7 @@ class Comment extends React.Component {
             <div className={"comment"}>
                 {/*<h1>Comment</h1>*/}
                 <img className={"comment-user-pic"} src={this.state.userpic}/>
-                <a href="" className={"comment-user"}>{this.props.data.username}</a>
+                <NavLink to={"/users/"+this.props.data.username} className={"comment-user"}>{this.props.data.username}</NavLink>
                 <div className={"comment-content"}>{this.props.data.content}</div>
             </div>
         );
